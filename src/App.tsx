b946844ecdd32d2c39aabd8b95e15aadf0ddc880
Aaron Pavlick <apavlick@yext.com>
2022-11-17 13:13:18 -0500

    removing notes from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,3 @@ function App() {
 }
 
 export default App;
-
-// Notes
-// 1. The issue where the background color was appearing black was due to the styles in main.css. There was a dark background color set on the body element and I removed most of the styles there.
-// 2. I did not set a vertical key in the in the HeadlessConfig in main.tsx because I want to use universal search when you land on the home page.
-// 3. I ran yext types generate search src/types --experienceKey test-kering to generated the types for the search experience.
